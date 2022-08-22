7abda34e2e66d69757cec05cb76f31332861755f
João Miguel <101208567+joao-panza@users.noreply.github.com>
2022-08-22 18:21:50 -0300

    adding hover to keys

--- main.js
+++ main.js
@@ -19,8 +19,55 @@ const multiply = document.querySelector('.asterisk.Key')
 const minus = document.querySelector('.minusKey')
 const sum = document.querySelector('.plusKey')
 
-//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
+// Zero button - SVG hover
 const zeroSvg = document.querySelector('.zeroSvg')
 zero.addEventListener("mouseover",()=>{zeroSvg.style.fill="black"})
 zero.addEventListener("mouseout",()=>{zeroSvg.style.fill="white"})
+
+// Equal button - SVG hover
+const equalSvg = document.querySelector('.equalSvg')
+equal.addEventListener("mouseover",()=>{equalSvg.style.fill="black"})
+equal.addEventListener("mouseout",()=>{equalSvg.style.fill="white"})
+
+// One button - SVG hover
+const oneSvg = document.querySelector('.oneSvg')
+one.addEventListener("mouseover",()=>{oneSvg.style.fill="black"})
+one.addEventListener("mouseout",()=>{oneSvg.style.fill="white"})
+
+// Two button - SVG hover
+const twoSvg = document.querySelector('.twoSvg')
+two.addEventListener("mouseover",()=>{twoSvg.style.fill="black"})
+two.addEventListener("mouseout",()=>{twoSvg.style.fill="white"})
+
+// Three button - SVG hover
+const threeSvg = document.querySelector('.threeSvg')
+three.addEventListener("mouseover",()=>{threeSvg.style.fill="black"})
+three.addEventListener("mouseout",()=>{threeSvg.style.fill="white"})
+
+// Plus button - SVG hover
+
+const plusSvg = document.querySelector('.plusSvg')
+sum.addEventListener("mouseover",()=>{plusSvg.style.fill="black"})
+sum.addEventListener("mouseout",()=>{plusSvg.style.fill="white"})
+
+// Four button - SVG hover
+
+const fourSvg = document.querySelector('.fourSvg')
+four.addEventListener("mouseover",()=>{fourSvg.style.fill="black"})
+four.addEventListener("mouseout",()=>{fourSvg.style.fill="white"})
+
+// Five button - SVG hover
+
+const fiveSvg = document.querySelector('.fiveSvg')
+five.addEventListener("mouseover",()=>{fiveSvg.style.fill="black"})
+five.addEventListener("mouseout",()=>{fiveSvg.style.fill="white"})
+
+// Six button - SVG hover
+
+const sixSvg = document.querySelector('.sixSvg')
+six.addEventListener("mouseover",()=>{sixSvg.style.fill="black"})
+six.addEventListener("mouseout",()=>{sixSvg.style.fill="white"})
+
+// Minus button - SVG hover
