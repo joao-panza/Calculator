8dddb91bf7425dfde3d24272d1f6fae6bb723247
João Miguel <joaomiguel.apr@gmail.com>
2022-08-22 04:33:29 -0300

    testing hover using js

--- main.js
+++ main.js
@@ -12,4 +12,15 @@ const nine = document.querySelector('.nineKey')
 const ce = document.querySelector('.ceKey')
 const c = document.querySelector('.cKey')
 const x = document.querySelector('.xKey')
+const equal = document.querySelector('.equalKey')
+
 const divide = document.querySelector('.divideKey')
+const multiply = document.querySelector('.asterisk.Key')
+const minus = document.querySelector('.minusKey')
+const sum = document.querySelector('.plusKey')
+
+//////////////////////////////////////////////////////////
+
+const zeroSvg = document.querySelector('.zeroSvg')
+zero.addEventListener("mouseover",()=>{zeroSvg.style.fill="black"})
+zero.addEventListener("mouseout",()=>{zeroSvg.style.fill="white"})
